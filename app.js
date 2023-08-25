03018b749614192326663d7e13ee472d99e5f610
Jeyavarthini <jeyavarthiniravi>
2023-08-25 16:20:29 +0530

    commit

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get("/", (req, res) => {
 
 // Connect to Database
 mongoose.connect(
-  "mongodb://hongly:vhvzx20h33QhYfwa@nodejs-cluster-shard-00-00-5aydg.mongodb.net:27017,nodejs-cluster-shard-00-01-5aydg.mongodb.net:27017,nodejs-cluster-shard-00-02-5aydg.mongodb.net:27017/test?ssl=true&replicaSet=nodejs-cluster-shard-0&authSource=admin&retryWrites=true&w=majority",
+  "mongodb+srv://Jeyavarthini:Princess03@cluster.rfgjhqq.mongodb.net/",
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => console.log("connected to database!")
 );
