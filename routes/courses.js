ac51a5ea4cd127d7dd82fdd4fa0e70d7a9ab55a3
Jeyavarthini <jeyavarthiniravi>
2023-08-26 14:41:59 +0530

    commit

--- routes/courses.js
+++ routes/courses.js
@@ -1,32 +1,40 @@
-// const express = require("express");
-// const router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-// const Course = require("../models/Course");
+const Course = require("../models/Course");
 
-// router.get("/", (req, res) => {
-//   res.send("Hi, we are now at courses route");
-// });
+router.get("/", (req, res) => {
+  res.send("Hi, we are now at courses route");
+});
 
-// router.get("/1", (req, res) => {
-//   res.send("I am now in course #1");
-// });
+router.get("/1", (req, res) => {
+  res.send("I am now in course #1");
+});
 
-// router.post("/", (req, res) => {
-//   const mycourse = new Course({
-//     course: req.body.course,
-//     tag: req.body.tag
-//   });
+router.post("/", (req, res) => {
+  const mycourse = new Course({
+    course: req.body.course,
+    tag: req.body.tag
+  });
 
-//   mycourse
-//     .save()
-//     .then(result => {
-//       //res.json(result);
-//       console.log(result);
-//     })
-//     .catch(err => {
-//       //res.json({ message: err });
-//       console.log(err);
-//     });
-// });
+  mycourse
+    .save()
+    .then(result => {
+      //res.json(result);
+      console.log(result);
+    })
+    .catch(err => {
+      //res.json({ message: err });
+      console.log(err);
+    });
+});
 
-// module.exports = router;
+module.exports = router;
+// const express =require("express");
+// const router=express.Router();
+
+// router.get("/api/courses",(req,res)=>{
+//     res.send("Hi unique!")
+// })
+
+// module.exports=router;
